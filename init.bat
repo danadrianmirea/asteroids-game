10707a4cb757b8e5ea612b6b593ab5bcd90fc093
Dan Adrian Mirea <damirea84@gmail.com>
2024-08-30 23:38:43 +0300

    remove init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# asteroids-game" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/asteroids-game.git
-git push -u origin main
